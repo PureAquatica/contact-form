12e736f3989925f17b8d538796047e8d28ca0716
pureaquatica <mat.duck@gmail.com>
2015-11-02 21:26:51 +0000

    changed root form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   # this is controller # action .
+   root 'form#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
